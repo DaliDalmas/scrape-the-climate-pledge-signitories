0a0c96ad4f74c3b3c6b2241a82b8bcfa572c13da
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2023-04-16 21:16:30 +0300

    done for real

--- clean.py
+++ clean.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 data = pd.read_csv('signatories.csv')
-names = data['signatory_sites'].apply(lambda link: str(link).split('.')[-2].split('/')[-1].replace('_', ' ').replace('-', ' ').upper())
+names = data['sign_link'].apply(lambda link: str(link).split('.')[-2].split('/')[-1].replace('_', ' ').replace('-', ' ').upper())
 data['signatory_names'] = names
-data.to_excel('clean_signatories.xlsx', index=False)
+data.to_excel('clean_signatories_final.xlsx')
